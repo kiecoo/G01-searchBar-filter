b577e352b3bc75b434c207c43a9251d043972a37
Michelle <kiecoomi@gmail.com>
2019-05-03 22:36:04 +0800

    UPDATE data of shops

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,58 @@
 const data = [
 	{
-		name: '七喜廚房',
-		logo: 'https://i.imgur.com/OiSniLG.jpg',
-		stack: [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag3', 'tag4' ]
+		"address" : "台中市中區三民路二段18巷6號",
+		"business_time" : "請注意粉絲頁",
+		"latitude" : 24.1422103,
+		"longitude" : 120.6589805,
+		"name" : "七喜廚房",
+		"phone" : "04-22211700",
+		"remark" : "源於七喜廚房，以續食推動零廢棄生活的實驗性聚落，希望在都市當中人與廢棄物共生的系統，讓萬物與人一樣沒有所謂的剩餘。",
+		"type" : "",
+		"url" : "https://www.facebook.com/7upkitchen/\nhttps://www.7ckitchen.com",
+		"logo": 'https://i.imgur.com/OiSniLG.jpg',
+		"stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag3', 'tag4' ]
 	},
 	{
-		name: '艸木川素食',
-		logo: 'https://i.imgur.com/YGKTNkA.jpg',
-		stack: [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag4' ]
-	}
+		"address" : "108台北市萬華區中華路二段334號",
+		"business_time" : "每週四1130~1300市場惜食分享, 每週五1500~1800共煮送餐交流",
+		"latitude" : 25.0989537,
+		"longitude" : 121.5081786,
+		"name" : "南機拌飯",
+		"phone" : "0963 615 016",
+		"remark" : "",
+		"type" : "",
+		"url" : "https://www.facebook.com/NanjiRice/",
+		"logo": 'https://i.imgur.com/YGKTNkA.jpg',
+		"stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste', 'tag3' ]
+	},
+	{
+		"address" : "950台東縣台東市漢陽北路169巷64弄26號",
+		"business_time" : "",
+		"latitude" : 22.74739,
+		"longitude" : 121.132759,
+		"name" : "迷走。回家旅人空間",
+		"phone" : 989360055,
+		"remark" : "",
+		"shop_type" : "",
+		"type" : "food_share",
+		"url" : "https://www.facebook.com/lostandfoundtaitung/\nhttps://jtwagor.wixsite.com/lostandfoundtaitung",
+		"logo": 'https://i.imgur.com/OiSniLG.jpg',
+		"stack":  [ 'tag1_Unpackaged', 'tag2_ZeroWaste' ]
+	},
+	{
+		"address" : "300新竹市東區光復路二段101號清華大學風雲樓一樓水木書苑內",
+		"business_time" : "週二~周日1000~1800",
+		"latitude" : 24.792323,
+		"longitude" : 120.9950025,
+		"name" : "水木咖啡 * 小客廳",
+		"phone" : "03 572 3474",
+		"remark" : "",
+		"type" : "",
+		"url" : "https://www.facebook.com/%E6%B0%B4%E6%9C%A8%E5%92%96%E5%95%A1-x-%E5%B0%8F%E5%AE%A2%E5%BB%B3-The-Living-Room-645497232164723/",
+		"logo": 'https://i.imgur.com/YGKTNkA.jpg',
+		"stack":  [ 'tag1_Unpackaged' ]
+	},
 ];
 
-export default data;
+  
+  export default data;
